6d19f849eb818567d143c4cf3f488bad5a31d5d8
Naman <naman1.garg@paytmpayments.com>
2026-06-30 00:03:04 +0530

    multiple transaction initiater

--- src/main/java/com/eventrouting/upstream/controller/TransactionController.java
+++ src/main/java/com/eventrouting/upstream/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.eventrouting.upstream.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eventrouting.upstream.dto.TransactionRequest;
@@ -17,8 +18,12 @@ public class TransactionController {
 	private final TransactionService transactionService;
 
 	@PostMapping("/transactions")
-	public ResponseEntity<Void> initiateTransaction(@RequestBody TransactionRequest request) {
-		transactionService.initiateTransaction(request);
+	public ResponseEntity<Void> initiateTransaction(
+			@RequestBody TransactionRequest request,
+			@RequestParam(name = "count", defaultValue = "1") int count) {
+		for (int i = 0; i < count; i++) {
+			transactionService.initiateTransaction(request);
+		}
 		return ResponseEntity.accepted().build();
 	}
 
